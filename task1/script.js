15e17b260f8a9a195be01e2ffe20f33aeafa869c
ArdiansyahPutraPinem <ardiansyahputrap@gmail.com>
2021-03-18 15:57:07 +0700

    initial commit

---
+++ task1/script.js
@@ -0,0 +1,30 @@
+window.doTriangle = function (form) {
+  n = parseInt(form.rows.value, 10);
+
+  let string = "";
+
+  for (let i = 1; i <= n; i++) {
+    // printing spaces
+    for (let j = n; j > i; j--) {
+      string += " ";
+    }
+    // printing star
+    for (let k = 0; k < i * 2 - 1; k++) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  // downside
+  for (let i = 1; i <= n - 1; i++) {
+    // printing spaces
+    for (let j = 0; j < i; j++) {
+      string += " ";
+    }
+    // printing star
+    for (let k = (n - i) * 2 - 1; k > 0; k--) {
+      string += "*";
+    }
+    string += "\n";
+  }
+  console.log(string);
+};
